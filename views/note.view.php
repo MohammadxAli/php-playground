f64bf0ec47453f61f333f3d0cb263bdfeb85a5ec
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2024-01-07 23:10:42 +0330

    Escape html tags when rendering `body`

--- views/note.view.php
+++ views/note.view.php
@@ -6,7 +6,7 @@
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <a href="/notes" class="inline-block mb-4 text-blue-500 underline">Go back...</a>
         <p>
-            <?= $note['body'] ?>
+            <?= htmlspecialchars($note['body']) ?>
         </p>
     </div>
 </main>
