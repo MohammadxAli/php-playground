3ba43194c55982ce16fd2fb33d5b01dcf2916c95
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2024-01-07 17:01:22 +0330

    Show 403 view if `user_id` doesn't match `$currentUserId`

--- controllers/note.php
+++ controllers/note.php
@@ -6,6 +6,12 @@ $config = require('config.php');
 
 $db = new Database($config['database']);
 
+$currentUserId = 2;
+
 $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+
+if ($note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 require 'views/note.view.php';
