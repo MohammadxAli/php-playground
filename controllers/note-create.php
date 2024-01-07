f8acf77fae9d8382bf56fc873b26d2bf64d14f25
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2024-01-07 23:11:34 +0330

    Wire up creating note operation to database

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -2,9 +2,17 @@
 <?php
 
 $title = 'Create Note';
+$config = require('config.php');
+$db = new Database($config['database']);
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+    $currentUserId = 2;
+
+    $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => $currentUserId,
+    ]);
 }
 
 require 'views/note-create.view.php';
