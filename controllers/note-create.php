f0d092003e5de8b4c01effca237d3da8366fa243
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2024-01-07 23:33:05 +0330

    Add a form validation to user's input in `body` field

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -8,11 +8,22 @@ $db = new Database($config['database']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $currentUserId = 2;
+    $errors = [];
 
-    $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => $currentUserId,
-    ]);
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required.';
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'Body can not be more than 1,000 characters long.';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => $currentUserId,
+        ]);
+    }
 }
 
 require 'views/note-create.view.php';
