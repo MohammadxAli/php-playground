aca49f9fd4739840f00fa96d228876c680765c0c
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2024-01-13 13:24:01 +0330

    Validate user's input on registration form

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -1,6 +1,18 @@
 <?php
+use Core\Validator;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
 
 $errors = [];
+
+if (!Validator::email($email)) {
+    $errors['email'] = 'Please enter a valid email address.';
+}
+
+if (!Validator::string($password, 7, 255)) {
+    $errors['password'] = 'Password should be at least 7 characters.';
+}
 
 if (!empty($errors)) {
     return view("registration/show", [
