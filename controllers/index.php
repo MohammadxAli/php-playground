51485c0802af1bb157390cd7d8aca9461c9ab492
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2024-01-13 13:01:52 +0330

    Setup a basic session for testing purposes

--- controllers/index.php
+++ controllers/index.php
@@ -1,5 +1,7 @@
 <?php
 
+$_SESSION['name'] = 'Ali';
+
 view('index', [
     'title' => 'Home'
 ]);
--- views/partials/banner.php
+++ views/partials/banner.php
@@ -1,5 +1,10 @@
 <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900"><?= $title ?></h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">
+            <?= $title ?>
+        </h1>
+        <p class="mt-2 opacity-70">Hello
+            <?= $_SESSION['name'] ?? 'Guest' ?>
+        </p>
     </div>
 </header>
