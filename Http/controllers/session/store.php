bc0456eea00d8d437c63bdbbc8d079b28d065f59
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2024-01-15 23:29:53 +0330

    Handle the login form validation in a better way

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -1,26 +1,21 @@
 <?php
 use Core\Authenticator;
 use Http\Forms\LoginForm;
-use Core\Session;
 
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-$form = new LoginForm();
-
-if ($form->validate($email, $password)) {
-    if ((new Authenticator)->attempt($email, $password)) {
-        redirect('/');
-    }
-
-    $message = 'The entered credentials are incorrect.';
-    $form->error('email', $message);
-    $form->error('password', $message);
-}
-
-Session::flash('errors', $form->errors());
-Session::flash('old', [
-    'email' => $email,
+$form = LoginForm::validate($attributes = [
+    'email' => $_POST['email'],
+    'password' => $_POST['password'],
 ]);
 
-redirect('/login');
+$isLoggedIn = (new Authenticator)->attempt($attributes['email'], $attributes['password']);
+
+if (!$isLoggedIn) {
+    $message = 'The entered credentials are incorrect.';
+
+    $form->error('email', $message)->error('password', $message)->throw();
+
+    redirect('/login');
+}
+
+redirect('/');
+
